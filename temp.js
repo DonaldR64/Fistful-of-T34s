ffac68d1c96c2261c58b399949bc4b7455b8b05a
DonaldR64 <don.rhodes@icloud.com>
2026-02-14 10:30:47 -0500

    a

--- temp.js
+++ temp.js
@@ -1,201 +1,116 @@
-const AdvancePhase = () => {
-    let turn = state.FFT.turn;
-    let phase = state.FFT.phase;
-    let phases = ["Deployment","Airstrikes & Area Fire","Movement","Close Combat","Direct Fire","End Phase"];
-    if (turn === 0) {
-        currentPhase = "Deployment";
+    const ArtilleryAvailability2 = (player) => {
+        artUnits = [];
+        let avail = [];
+        let unavail = [];
         _.each(UnitArray,unit => {
-            if (HexMap[unit.hexLabel].offboard === false) {
-                unit.token.set("tint_color","#000000");
+            if (unit.player === player) {
+                if (unit.artFlag === true && unit.CheckSuppression() === false) {
+                    let availMod = unit.avail;
+                    let tipmods = "";
+                    if (availMod !== 0) {
+                        tipmods += "<br>Availability: " + availMod;
+                    }
+                    let offboard = HexMap[unit.hexLabel].offboard;
+                    if (offboard === false && target > 1) {
+                        availMod++;
+                        tipmods += "<br>On Board Artillery +1";
+                    }         
+                    let result = ArtAvailTable[unit.nation,unit.artType,availMod];
+
+                    tip = "Final Result: " + result.result;
+                    tip = "Roll: " + result.roll + tipmods;
+
+
+                    tip = '[🎲 ](#" class="showtip" title="' + tip + ')';
+                    if (result.FU > 0) {
+                        unit.FU = result.FU;
+                        artUnits.push(unit);
+                        avail.push(tip + unit.name + " is Available with " + result.FU + " FU");
+                    } else {
+                        if (unit.type === "Aircraft") {
+                            unavail.push(tip + unit.name + " is Refuelling/Reloading");
+                        }
+                        if (unit.type === "Artillery" && offboard === true) {
+                            let roll = randomInteger(6);
+                            if (roll < 4) {
+                                unavail.push(tip + unit.name + " is Unavailable");
+                            } else {
+                                unavail.push(tip + unit.name + " is Reloading");
+                            }
+                        }
+                        if (offboard === false) {
+                            unavail.push(tip + unit.name + " is Reloading and Unavailable");
+                        }
+                        unit.token.set(SM.unavail,true);
+                    }
+                }
             }
-            //everybody is not spotted
         })
-    } else {
-        phaseNum = phases.indexOf(phase) + 1;
-        if (phaseNum >= phases.length) {
-            phaseNum = 0
-            activePlayer = activePlayer === 0 ? 1:0;
-            turn++;
+        if (avail.length > 0) {
+            for (let i=0;i<avail.length;i++) {
+                outputCard.body.push(avail[i]);
+            }
+            if (unavail.length > 0) {
+                outputCard.body.push("[hr]");
+            }
         }
-        currentPhase = phases[phaseNum];
-    }
-
-    state.FFT.turn = turn;
-    state.FFT.activePlayer = activePlayer;
-    state.FFT.currentPhase = currentPhase;
-
-    SetupCard(currentPhase,"Turn " + turn,state.FFT.nations[currentPlayer]);
-
-    switch(currentPhase) {
-        case "Deployment":
-            DeploymentPhase();
-            break;
-        case "Airstrikes & Area Fire":
-            AreaFirePhase();
-            break;
-        case "Movement":
-            MovementPhase();
-            break;
-        case "Close Combat":
-            FirstQC();
-            break;
-        case "Direct Fire":
-            DirectFirePhase();
-            break;
-        case "End Phase":
-            SecondQC();
-            break;
-    }
-    
-    PrintCard();
-
-}
-
-const DeploymentPhase = () => {
-    outputCard.body.push("Deploy any Available Reinforcements");
-    outputCard.body.push("No Overwatch Fire is allowed");
-}
-
-const AreaFirePhase = () => {
-    _.each(UnitArray,unit => {
-        unit.token.set(SM.fired,false);
-        unit.token.set(SM.unavail,false);
-        unit.token.set(SM.move,false);
-        unit.token.set(SM.double,false);
-        unit.token.set(SM.down,false);
-        if (unit.player !== activePlayer) {
-            unit.Suppress("A",false);
+        if (avail.length === 0) {
+            outputCard.body.push("No Artillery or Air Support");
         }
-        if (unit.player === activePlayer) {
-            unit.SetOverwatch(false);
-            let m = unit.movement;
-            if (unit.CheckSuppression()) {m -= 2};
-            unit.token.set("bar1_value",m);
-        }
-
-        unit.startRotation = unit.token.get("rotation");
-        unit.startHexLabel = unit.hexLabel;
-        unit.spotter = false;
-        unit.artQC = false;
-        unit.coverTest = false;
-    })
-    //remove smoke
-    _.each(HexMap,hex => {
-        if (hex.smokePlayer === activePlayer) {
-            let smokeID = hex.smoke;
-            let smoke = findObjs({_type:"graphic", id: smokeID})[0];
-            smoke.remove();
-            hex.smoke = "";
-            hex.smokePlayer = "";
-        }
-    })
-
-    outputCard.body.push("[U]Available Artillery[/u]");
-    ArtilleryAvailability(activePlayer);
-    outputCard.body.push("[hr]");
-    outputCard.body.push("Place and resolve any Area Fire attacks");
-    outputCard.body.push("No Overwatch Fire is allowed");
-}
-
-const MovementPhase = () => {
-    outputCard.body.push("Overwatch Fire can occur at any time");
-    outputCard.body.push("Quality Checks will be done at the end of the Phase");
-}
-
-const FirstQC = () => {
-    //qc from overwatch, note on unit somewhere has passed, ? a green dot
-    QCCheck(); //builds array qcUnits
-    nextPhase = "CloseCombat";
-    RunQC(); //checks it, when 'empty' feed to nextPhase
-}
-
-const CloseCombatPhase = () => {
-    //qc from overwatch, note on unit somewhere has passed, ? a green dot
-    outputCard.body.push("Resolve Close Combats");
-    outputCard.body.push("The Active Player determines the order of the Close Combats");
-}
-
-const DirectFirePhase = () => {
-    //spotting from movement phase
-    _.each(UnitArray,unit => {
-        if (unit.player !== activePlayer) {
-            unit.Suppress("B",false);
-        }
-    })
-    outputCard.body.push("Overwatch Fire must take place BEFORE the Active Player begins their own fire");
-    outputCard.body.push("Quality Checks will be done at the end of the Phase");
-}
-
-const SecondQC = () => {
-    //qc from Direct Fire
-    QCCheck(); //builds array qcUnits
-    nextPhase = "FormQC";
-    RunQC(); //checks it, when 'empty' feed to nextPhase
-}
-
-const StartFormQC = () => {
-    //check formations for their quality checks if warranted
-    let qcFormations = [];
-    _.each(FormationArray, formation => {
-        if (formation.casualties >= formation.breakpoint) {
-            qcFormations.push(formation);
-        }
-    })
-    RunFormQC();
-}
-
-const EndPhase = () => {
-    //place any unit of active players that didnt move or fire on overwatch
-    _.each(UnitArray,unit => {
-        if (unit.token.get(SM.fired) === false && unit.token.get(SM.move) === false && unit.token.get(SM.double) === false && unit.player === activePlayer && unit.token.get(SM.unavail) === false) {
-            //sets overwatch
-            unit.SetOverwatch(true);
-        }
-    })
-    //spotting for end phase
-    CheckSpotting(unit,"End")
-}
-
-const RunQC = () => {
-    let unit = qcUnits.shift();
-    if (unit) {
-        sendPing(unit.token.get("left"),unit.token.get("top"),Campaign().get("playerpageid"),null,true);
-        SetupCard(unit.name,"Quality Check",unit.nation);
-        ButtonInfo("Make Quality Check","!TakeQC;" + unit.id);
-        PrintCard();
-    } else {
-        if (nextPhase === "CloseCombat") {
-                CloseCombatPhase();
-        } else if (nextPhase === "EndPhase") {
-            EndPhase();
-        } else if (nextPhase === "FormQC") {
-            StartFormQC();
-        } else {
-            sendChat("","??")
+        if (unavail.length > 0) {
+            for (let i=0;i<unavail.length;i++) {
+                outputCard.body.push(unavail[i]);
+            }
         }
     }
-}
 
-const QCCheck = () => {
-    qcUnits = [];
-        _.each(UnitArray,unit => {
-        if (unit.token.get(SM.qc)) {
-            qcUnits.push(unit);
-        }
-    })
-}
 
-const RunFormQC = () => {
-    let formation = qcFormations.shift();
-    if (formation) {
-        let unit = UnitArray[formation.tokenIDs[0]];
-        if (unit) {
-            sendPing(unit.token.get("left"),unit.token.get("top"),Campaign().get("playerpageid"),null,true);
-            SetupCard(formation.name,"Formation QC",unit.nation);
-            ButtonInfo("Make Quality Check","!TakeFQC;" + unit.id);
-            PrintCard();
+
+
+    const ArtAvailTable = (nation,type,availMod) => {
+        let side = {
+            "Wermacht": "Western",
+            "Red Army": "Soviet",
         }
-    } else {
-       EndPhase();
+
+        let FUTable = {
+            "Western": {
+                "Self-Propelled": [0,1,1,2,2,3],
+                "Towed": [0,0,1,1,2,3],
+                "Battalion": [0,1,1,1,1,1,],
+            },
+            "Soviet": {
+                "Self-Propelled": [0,0,1,2,3,3],
+                "Towed": [0,0,0,1,3,3],
+                "Battalion": [0,0,1,1,1,1,],
+            },
+        }
+        let artRoll = randomInteger(6);
+        let artResult = artRoll + availMod;
+        artResult = Math.min(Math.max(0,artResult),6);
+        let FU = FUTable[side[nation]][type][artResult - 1];
+        let result = {
+            roll: artRoll,
+            result: artResult,
+            FU: FU,
+        }
+        return result;
     }
-}
+
+    const AreaFireIndexTable = (fu,calibre) => {
+
+
+
+
+
+
+    }
+
+    const MRLSAreaFireIndexTable = (fu,size) => {
+
+
+
+
+
+        
+    }
