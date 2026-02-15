f20516efee87f35f1144f97f50f6fd287b035941
DonaldR64 <don.rhodes@icloud.com>
2026-02-15 16:30:45 -0500

    a

--- fistful.js
+++ fistful.js
@@ -84,7 +84,8 @@ const FFT = (() => {
     let currentPhase = state.FFT.phase || "Deployment";
     let nextPhase = "";
     let MapAreas = {};
-
+    let areaFire = {};
+    let areaFireList = [];
 
     let outputCard = {title: "",subtitle: "",side: "",body: [],buttons: [],};
 
@@ -712,7 +713,7 @@ const FFT = (() => {
             } else {
                 artRange = [0,parseInt(artRange)];
             }
-            this.artrange = artRange;
+            this.artRange = artRange;
 
             let artType = aa.arttype;
             if (artType) {artType = artType.replace(" Artillery","")};
@@ -2012,7 +2013,7 @@ log(unit.name)
 
 
 
-    const CallArtillery = (msg) => {
+    const CallArtilleryONe = (msg) => {
         let spotterID = msg.selected[0]._id;
         let spotter = UnitArray[spotterID];
         SetupCard(spotter.name,"Call Artillery",spotter.nation);
@@ -2033,6 +2034,8 @@ log(unit.name)
         }
         PrintCard();
     }
+
+
 
 
     const CheckArtillery = () => {
@@ -3590,7 +3593,7 @@ log("Phase" + currentPhase)
             AreaFirePhase();
             break;
         case "Movement":
-            MovementPhase();
+            ResolveAreaPhase();
             break;
         case "Close Combat":
             FirstQC();
@@ -3650,10 +3653,23 @@ const AreaFirePhase = () => {
     outputCard.body.push("[U]Available Artillery[/u]");
     ArtilleryAvailability2(activePlayer);
     outputCard.body.push("[hr]");
-    outputCard.body.push("Place and resolve any Area Fire attacks");
+    outputCard.body.push("Place Air and Artillery");
     outputCard.body.push("No Overwatch Fire is allowed");
     PrintCard();
 }
+
+const ResolveAreaPhase = () => {
+    areaFire = areaFireList.shift();
+    if (areaFire) {
+        sendPing(areaFire.centre.x,areaFire.centre.y,Campaign().get("playerpageid"),null,true);
+        SetupCard("Area Fire","",state.FFT.nations[activePLayer]);
+        ButtonInfo("Resolve","!AreaFire");
+        PrintCard();
+    } else {
+        MovementPhase();
+    }
+}
+
 
 const MovementPhase = () => {
     RemoveDead();
@@ -3798,6 +3814,152 @@ const RunFormQC = () => {
         }
         PrintCard();
     }
+
+const CallArtillery = (msg) => {
+    let spotterID = msg.selected[0]._id;
+    let spotter = UnitArray[spotterID];
+    SetupCard(spotter.name,"Call Artillery",spotter.nation);
+    if (((spotter.special.includes("Recon") || spotter.special.includes("Forward Observer")) && spotter.spotter >= 2) ||  (spotter.special.includes("Recon") === false && spotter.special.includes("Forward Observer") === false && spotter.spotter >= 1)) {
+        outputCard.body.push("Unit unable to Spot for any more Barrages");
+    } else if (artUnits.length === 0) {
+        outputCard.body.push("There is no Available Artillery or Airstrikes");
+    } else {
+        CreateArtilleryToken(spotter);
+        outputCard.body.push("Place Target and Select Artillery");
+        outputCard.body.push("Commit when done");
+    }
+    PrintCard();
+}
+
+const CreateArtilleryToken = (spotter) => {
+    let centre = HexMap[spotter.hexLabel].centre;
+    
+    let img = getCleanImgSrc("https://files.d20.io/images/105823565/P035DS5yk74ij8TxLPU8BQ/thumb.png?1582679991");
+    let charID = "-OkLrIEzBQrYJMzCEg5H";
+    let existing = findObjs({_type:"graphic", represents: charID});
+    _.each(existing,tok => {
+        let exist = UnitArray[tok.get("id")];
+        if (exist) {
+            delete UnitArray[tok.get("id")];
+        }
+        tok.remove();
+    })
+
+    let newToken = createObj("graphic", {
+        left: centre.x,
+        top: centre.y,
+        width: 80,
+        height: 80, 
+        pageid: Campaign().get("playerpageid"),
+        imgsrc: img,
+        layer: "objects",
+        represents: charID,
+        name: "Artillery Target",
+        showname: true,
+        disableTokenMenu: true,
+        showplayers_aura1: true,
+        gmn: "TargetIcon",
+    })
+    toFront(newToken);
+    //redo ability
+    let abilArray = findObjs({_type: "ability", _characterid: charID});
+    //clear old abilities
+    for(let a=0;a<abilArray.length;a++) {
+        abilArray[a].remove();
+    } 
+    //create a macro for each avail artillery unit, labelled 1 - name etc
+    for (let i=0;i<artUnits.length;i++) {
+        let abilityName = (i+1) + ": " + artUnit.name;
+        let action = "!AddArtillery;" + newToken.id + ";" + spotter.id + ";" + artUnit.id + ";?{Type|HE|Smoke";
+        AddAbility(abilityName,action,charID);
+    }
+    //then add a macro for check LOS, and a macro for Commit and Cancel
+    abilityName = "Check LOS";
+    action = "!CheckLOS;" + spotter.id + ";" + newToken.id;
+    AddAbility(abilityName,action,charID);
+    abilityName = "Commit";
+    action = "!CommitArtillery";
+    AddAbility(abilityName,action,charID);
+    abilityName = "Cancel";
+    action = "!CancelArtillery";
+    AddAbility(abilityName,action,charID);
+    areaFire = {
+        centre: "",
+        artUnits: [],
+        type: "",
+    };
+}
+
+const AddArtillery = (msg) => {
+    let Tag = msg.content.split(";");
+    let targetID = Tag[1];
+    let target = UnitArray[targetID];
+    let spotterID = Tag[2];
+    let spotter = UnitArray[spotterID];
+    let artilleryID = Tag[3];
+    let artillery = UnitArray[artilleryID];
+    let type = Tag[4]; //HE Or Smoke
+
+    //check LOS first
+    let losResult = LOS(spotter,target);
+    if (losResult.los === false) {
+        sendChat("","No LOS To Target")
+        return;
+    }
+    //mixing HE and SMoke ?
+    if (areaFire.type === "") {
+        areaFire.type = type;
+    } else if (areaFire.type !== type) {
+        sendChat("","Cannot mix HE and Smoke in same Barrage");
+        return;
+    }
+    //check range for the artillery selected
+    if (HexMap[artillery.hexLabel].offboard === false) {
+        let distance = artillery.Distance(target);
+        if (distance < artillery.artRange[0] || distance > artillery.artRange[1]) {
+            sendChat("","Not in Range of Selected Artillery");
+            return;
+        }
+    } 
+    //lock target token in place
+    if (areaFire.centre === "") {
+        areaFire.centre = HexMap[target.hexLabel].centre;
+        target.token.set("lockMovement",true);
+    }
+    areaFire.units.push(artillery);
+    sendChat("",artillery.name + " Added to Barrage");
+}
+
+
+const CommitArtillery = (msg) => {
+    areaFireList.push(areaFire);
+    areaFire = {};
+    sendChat("","Committed");
+    let tokenID = msg.selected[0]._id;
+    let token =  findObjs({_type:"graphic", id: tokenID})[0];
+    let targetUnit = UnitArray[tokenID];
+    if (token) {
+        token.remove()
+    }
+    if (targetUnit) {
+        delete UnitArray[tokenID];
+    }
+}
+
+const CancelArtillery = (msg) => {
+    areaFire = {
+        centre: "",
+        artUnits: [],
+        type: "",
+    };
+    sendChat("","Cleared all Artillery Attached to this Target");
+    let tokenID = msg.selected[0]._id;
+    let token =  findObjs({_type:"graphic", id: tokenID})[0];
+    token.set("lockMovement",false);
+}
+
+
+
 
 
 
@@ -4170,8 +4332,15 @@ log("Final Hex Label: " + finalHexLabel)
             case '!Transport':
                 Transport(msg);
                 break;
-
-
+            case '!AddArtillery':
+                AddArtillery(msg);
+                break;
+            case '!CommitArtillery':
+                CommitArtillery(msg);
+                break;
+            case '!CancelArtillery':
+                CancelArtillery(msg);
+                break;
         }
     };
 
--- temp.js
+++ temp.js
@@ -1,120 +1,142 @@
-    const ArtilleryAvailability2 = (player) => {
-        artUnits = [];
-        let avail = [];
-        let unavail = [];
-        _.each(UnitArray,unit => {
-            if (unit.player === player) {
-                if (unit.artFlag === true && unit.CheckSuppression() === false) {
-                    let availMod = unit.avail;
-                    let tipmods = "";
-                    if (availMod !== 0) {
-                        tipmods += "<br>Availability: " + availMod;
-                    }
-                    let offboard = HexMap[unit.hexLabel].offboard;
-                    if (offboard === false && target > 1) {
-                        availMod++;
-                        tipmods += "<br>On Board Artillery +1";
-                    }         
-                    let result = ArtAvailTable[unit.nation,unit.artType,availMod];
-
-                    tip = "Final Result: " + result.result;
-                    tip = "Roll: " + result.roll + tipmods;
-
-
-                    tip = '[🎲 ](#" class="showtip" title="' + tip + ')';
-                    if (result.FU > 0) {
-                        unit.FU = result.FU;
-                        artUnits.push(unit);
-                        avail.push(tip + unit.name + " is Available with " + result.FU + " FU");
-                    } else {
-                        if (unit.type === "Aircraft") {
-                            unavail.push(tip + unit.name + " is Refuelling/Reloading");
-                        }
-                        if (unit.type === "Artillery" && offboard === true) {
-                            let roll = randomInteger(6);
-                            if (roll < 4) {
-                                unavail.push(tip + unit.name + " is Unavailable");
-                            } else {
-                                unavail.push(tip + unit.name + " is Reloading");
-                            }
-                        }
-                        if (offboard === false) {
-                            unavail.push(tip + unit.name + " is Reloading and Unavailable");
-                        }
-                        unit.token.set(SM.unavail,true);
-                    }
-                }
-            }
-        })
-        if (avail.length > 0) {
-            for (let i=0;i<avail.length;i++) {
-                outputCard.body.push(avail[i]);
-            }
-            if (unavail.length > 0) {
-                outputCard.body.push("[hr]");
-            }
-        }
-        if (avail.length === 0) {
-            outputCard.body.push("No Artillery or Air Support");
-        }
-        if (unavail.length > 0) {
-            for (let i=0;i<unavail.length;i++) {
-                outputCard.body.push(unavail[i]);
-            }
-        }
+const CallArtillery = (msg) => {
+    let spotterID = msg.selected[0]._id;
+    let spotter = UnitArray[spotterID];
+    SetupCard(spotter.name,"Call Artillery",spotter.nation);
+    if (((spotter.special.includes("Recon") || spotter.special.includes("Forward Observer")) && spotter.spotter >= 2) ||  (spotter.special.includes("Recon") === false && spotter.special.includes("Forward Observer") === false && spotter.spotter >= 1)) {
+        outputCard.body.push("Unit unable to Spot for any more Barrages");
+    } else if (artUnits.length === 0) {
+        outputCard.body.push("There is no Available Artillery or Airstrikes");
+    } else {
+        CreateArtilleryToken(spotter);
+        outputCard.body.push("Place Target and Select Artillery");
+        outputCard.body.push("Commit when done");
     }
+    PrintCard();
+}
 
-
-
-
-    const ArtAvailTable = (nation,type,availMod) => {
-        let side = {
-            "Wermacht": "Western",
-            "Red Army": "Soviet",
+const CreateArtilleryToken = (spotter) => {
+    let centre = HexMap[spotter.hexLabel].centre;
+    
+    let img = getCleanImgSrc("https://files.d20.io/images/105823565/P035DS5yk74ij8TxLPU8BQ/thumb.png?1582679991");
+    let charID = "-OkLrIEzBQrYJMzCEg5H";
+    let existing = findObjs({_type:"graphic", represents: charID});
+    _.each(existing,tok => {
+        let exist = UnitArray[tok.get("id")];
+        if (exist) {
+            delete UnitArray[tok.get("id")];
         }
+        tok.remove();
+    })
 
-        let FUTable = {
-            "Western": {
-                "Self-Propelled": [0,1,1,2,2,3],
-                "Towed": [0,0,1,1,2,3],
-                "Battalion": [0,1,1,1,1,1,],
-            },
-            "Soviet": {
-                "Self-Propelled": [0,0,1,2,3,3],
-                "Towed": [0,0,0,1,3,3],
-                "Battalion": [0,0,1,1,1,1,],
-            },
-        }
-        let artRoll = randomInteger(6);
-        let artResult = artRoll + availMod;
-        artResult = Math.min(Math.max(0,artResult),6);
-        let FU = FUTable[side[nation]][type][artResult - 1];
-        let result = {
-            roll: artRoll,
-            result: artResult,
-            FU: FU,
-        }
-        return result;
+    let newToken = createObj("graphic", {
+        left: centre.x,
+        top: centre.y,
+        width: 80,
+        height: 80, 
+        pageid: Campaign().get("playerpageid"),
+        imgsrc: img,
+        layer: "objects",
+        represents: charID,
+        name: "Artillery Target",
+        showname: true,
+        disableTokenMenu: true,
+        showplayers_aura1: true,
+        gmn: "TargetIcon",
+    })
+    toFront(newToken);
+    //redo ability
+    let abilArray = findObjs({_type: "ability", _characterid: charID});
+    //clear old abilities
+    for(let a=0;a<abilArray.length;a++) {
+        abilArray[a].remove();
+    } 
+    //create a macro for each avail artillery unit, labelled 1 - name etc
+    for (let i=0;i<artUnits.length;i++) {
+        let abilityName = (i+1) + ": " + artUnit.name;
+        let action = "!AddArtillery;" + newToken.id + ";" + spotter.id + ";" + artUnit.id + ";?{Type|HE|Smoke";
+        AddAbility(abilityName,action,charID);
     }
-
-
-
-    const AreaFireIndexTable = {
-        //ref by calibre first, then FU to get fire index #
-        //this only goes up to FU 10, and no ICM or Helo yet
-        "HE 30-69": [0,1,2,3,4,5,6,6,8,8,8],
-        "HE 70-89": [0,3,4,7,8,9,10,10,15,15,15],
-        "HE 90-119": [0,3,5,8,9,11,14,14,15,15,15],
-        "HE 120-139": [0,8,10,14,15,16,18,18,19,19,19],
-        "HE 140-169": [0,8,12,14,16,17,18,18,20,20,20],
-        "HE 170+": [0,14,16,18,19,21,21,21,22,22,22],
+    //then add a macro for check LOS, and a macro for Commit and Cancel
+    abilityName = "Check LOS";
+    action = "!CheckLOS;" + spotter.id + ";" + newToken.id;
+    AddAbility(abilityName,action,charID);
+    abilityName = "Commit";
+    action = "!CommitArtillery";
+    AddAbility(abilityName,action,charID);
+    abilityName = "Cancel";
+    action = "!CancelArtillery";
+    AddAbility(abilityName,action,charID);
+    areaFire = {
+        centre: "",
+        artUnits: [],
+        type: "",
     };
+}
 
-    const MRLSAreaFireIndexTable = {
-        //ref by size, and maxes at 10 FU
-        //ICM not in yet
-        "Light HE": [0,1,1,1,1,1,1,2,2,3,3],
-        "Medium HE": [0,1,1,2,4,5,6,8,8,9,10],
-        "Heavy HE": [0,8,12,15,16,18,19,19,21,21,21],
-        "Very Heavy HE": [0,12,15,17,18,19,21,21,22,22,22],
+const AddArtillery = (msg) => {
+    let Tag = msg.content.split(";");
+    let targetID = Tag[1];
+    let target = UnitArray[targetID];
+    let spotterID = Tag[2];
+    let spotter = UnitArray[spotterID];
+    let artilleryID = Tag[3];
+    let artillery = UnitArray[artilleryID];
+    let type = Tag[4]; //HE Or Smoke
+
+    //check LOS first
+    let losResult = LOS(spotter,target);
+    if (losResult.los === false) {
+        sendChat("","No LOS To Target")
+        return;
     }
+    //mixing HE and SMoke ?
+    if (areaFire.type === "") {
+        areaFire.type = type;
+    } else if (areaFire.type !== type) {
+        sendChat("","Cannot mix HE and Smoke in same Barrage");
+        return;
+    }
+    //check range for the artillery selected
+    if (HexMap[artillery.hexLabel].offboard === false) {
+        let distance = artillery.Distance(target);
+        if (distance < artillery.artRange[0] || distance > artillery.artRange[1]) {
+            sendChat("","Not in Range of Selected Artillery");
+            return;
+        }
+    } 
+    //lock target token in place
+    if (areaFire.centre === "") {
+        areaFire.centre = HexMap[target.hexLabel].centre;
+        target.token.set("lockMovement",true);
+    }
+    areaFire.units.push(artillery);
+    sendChat("",artillery.name + " Added to Barrage");
+}
+
+
+const CommitArtillery = (msg) => {
+    areaFireList.push(areaFire);
+    areaFire = {};
+    sendChat("","Committed");
+    let tokenID = msg.selected[0]._id;
+    let token =  findObjs({_type:"graphic", id: tokenID})[0];
+    let targetUnit = UnitArray[tokenID];
+    if (token) {
+        token.remove()
+    }
+    if (targetUnit) {
+        delete UnitArray[tokenID];
+    }
+}
+
+const CancelArtillery = (msg) => {
+    areaFire = {
+        centre: "",
+        artUnits: [],
+        type: "",
+    };
+    sendChat("","Cleared all Artillery Attached to this Target");
+    let tokenID = msg.selected[0]._id;
+    let token =  findObjs({_type:"graphic", id: tokenID})[0];
+    token.set("lockMovement",false);
+}
